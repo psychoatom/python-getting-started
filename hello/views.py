4236441c12078cd9ce615505edfc0d48c1e100d5
psychoatom <kelvinteo04@gmail.com>
2021-04-01 14:05:11 +0800

    update

--- hello/views.py
+++ hello/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
-import requests
+
+# import requests
+
+# Create your views here.
+# def index(request):
+#    r = requests.get('http://httpbin.org/status/418')
+#    print(r.text)
+#    return HttpResponse('<pre>' + r.text + '</pre>')
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
 
 
 def db(request):
